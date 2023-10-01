41c5725d5e7d6e510c98ea021b0eeb37d6535f09
Dbenjamin01 <125311954+Dbenjamin01@users.noreply.github.com>
2023-10-01 14:39:16 -0400

    Update UDPServer.py
    Cleaned up solution

--- UDPServer.py
+++ UDPServer.py
@@ -1,44 +1,26 @@
-#UDP-Server python file
-import socket
-from PIL import Image
+#  UDP-Server python file
 
-# identify serverPort, create UDP socket, bind socket to port
+import socket
+
+# Identify serverPort, create UDP socket, bind socket to port
 serverPort = 12500
-imagebytes = bytearray()
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSocket.bind(("", serverPort))
 
 # print ready message
+print("Waiting for number of Packets...")
+numPackets = serverSocket.recv(1024)  # Get number of packets to determine length of for-loop.
+print(f"Number of Packets: {numPackets.decode()}")
+
 print("The server is ready to receive.")
+file = open('serverside_image.bmp', "wb")  # Open file and write as binary
 
 while True:
-    # message, clientAddress = serverSocket.recvfrom(2048)
-    loopcounter = 0 #loop counter that checks the while loop when adding broken up byte array to server-side array
+    
+    for i in range(0, int(numPackets.decode())):
+        byte = serverSocket.recv(1024)  # Store packet info into byte variable
+        file.write(byte)  # Write received binary packet info into file
 
-    byte, clientAddress = serverSocket.recvfrom(2048) #receive byte from client
-    if not byte: # if the server is no longer receiving data from the client, exit the receive loop
-        #TODO: Loop should be exiting here, figure out why it isn't.
-        break
-
-
-    while loopcounter < len(byte): # byte comes in form of bytes. Bytes can be accessed like an array so go through byte[0]... byte[1]... and add them to an existing array
-        imagebytes.append(byte[loopcounter])
-        loopcounter += 1
+    file.close()  # Close file and allows user to open through OS
 
 
-    # byte is received from the client, begin reconstructing the image within the server here.
-    print("Byte received...")
-    print("Array Size: ")
-    print(len(imagebytes))
-
-print("Loop exited")
-
-#TODO Exit the loop, add the bytes recevied from client and create an image out of them through PIL library
-# repair image here, add the data together and create/show the image
-img = Image.Image.frombytes("L", imagebytes)
-
-# created image, show image here
-print("Image Created")
-
-#TODO Show image.
-img.show()
