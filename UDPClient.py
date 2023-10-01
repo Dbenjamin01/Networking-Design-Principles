50b9197d1438951bbc520e730e13d8350f498461
Dbenjamin01 <125311954+Dbenjamin01@users.noreply.github.com>
2023-10-01 14:39:38 -0400

    Update UDPClient.py
    Cleaned up solution

--- UDPClient.py
+++ UDPClient.py
@@ -1,55 +1,44 @@
-#UDP-Client python file
+#  UDP-Client python file
 import socket
-import os
-import time
-from PIL import Image
+import os  # Used to create file statistics such as name
+import time  # Used to sleep threads in real time
+import math  # Used to fix rounding error
 
-
-#Use similar setup for communication in phase 1
-
-# create server name, identify server port, and create UDP socket for server
+# Create server name, identify server port, and create UDP socket for server
 serverName = '127.0.0.1'
 
-# clients should use an ephemeral  port and CONNECT to the server port.
+# Clients should use an ephemeral  port and CONNECT to the server port.
 ephemeralPort = (serverName,12000)  # ref: https://stackoverflow.com/questions/68548331/client-and-server-connection-with-different-port-numbers
-serverPort = 12500  # identify the server port within the client, but have the client initialize its own port
+serverPort = 12500  # Identify the server port within the client, but have the client initialize its own port
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # import image using relative directory of image file path
 # "bitmap-img.bmp" <- file name
-imgname = "bitmap-img.bmp" # ref: https://www.digitalocean.com/community/tutorials/how-to-get-file-size-in-python
+imgName = "bitmap-img.bmp"  # ref: https://www.digitalocean.com/community/tutorials/how-to-get-file-size-in-python
 img = open("bitmap-img.bmp", "rb")
-file_statistics = os.stat(imgname)
-numbPackets = int((file_statistics.st_size / 1024 + 1))
+file_statistics = os.stat(imgName)
+numbPackets = int(math.ceil((file_statistics.st_size / 1024)))  # This ensures that there is room for remaining datagram
+                                                                # to be sent if division does not result in int
 
-clientSocket.sendto((str(numbPackets).encode()), (serverName, serverPort))# send file size to server to prepare for loop for receiving data.
-time.sleep(3)
-# img.show() test to verify that the image is properly found and imported into pycharm, is working.
+clientSocket.sendto((str(numbPackets).encode()), (serverName, serverPort))  # Send file size to server to prepare for loop for receiving data.
+time.sleep(3)  # Sleep thread to allow time for server to process information
 
-# simulate conversion of the file into packets to send over to the server.
-
-# to do so, break up the file into bytes, and send each byte to the server
-
-byte = img.read(1024)
+byte = img.read(1024)  # Creates packet size of 1024
 
 print("Sending bytes to Server...")
 while byte:
-    # send the outstanding byte
+    # Send the outstanding byte
     # once sent, get a new byte, of size 1024, from the image,
     # repeat loop until there are no bytes left within the image.
 
-    # we must send by one byte at a time.
+    # Must send by one byte at a time.
     clientSocket.sendto(byte, (serverName, serverPort))
     byte = img.read(1024)
 
-
-# await reply
 print("No more bytes left to send")
 img.close()
 clientSocket.close()
-# read reply characters from previously created socket into string, print string
-# modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-# print("From server: ", modifiedMessage.decode())
+
 
 
