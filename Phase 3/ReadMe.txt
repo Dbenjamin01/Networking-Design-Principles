7bf8091fffc4dc087b1a2260eed43c167350978c
Harrison <gillespie.harrison@gmail.com>
2023-10-24 17:58:54 -0400

    added some randomness code

--- Phase 3/ReadMe.txt
+++ Phase 3/ReadMe.txt
@@ -1,3 +1,4 @@
 Run using python phase_3.py
 View help: python phase_3.py -h
 Percent data corruption and ack corruption passed to Client/Server classes as integers
+Some initial code for the randomness, will need to be changed
--- Phase 3/phase_3.py
+++ Phase 3/phase_3.py
@@ -4,6 +4,7 @@ from time import sleep
 import argparse
 import os
 import math
+import random
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-a", "--ack", type=int, default=0)
@@ -35,13 +36,17 @@ class Server(Thread):
         self.server_socket = socket(AF_INET, SOCK_DGRAM)
         self.server_socket.bind(address)
         self.packet_size = 1024
-        self.data_err = data_err
+        self.data_err = data_err / 100
         self.image = "penguin.bmp"
 
     def send_image(self):
         packet = b''
+        err_pkts = []
+        counter = 0
         with open(self.image, 'rb') as img:
             num_pkts = str(int(math.ceil(os.fstat(img.fileno()).st_size / self.packet_size))).encode()
+            # List of packets when to send bad data:
+            err_pkts = random.sample(range(1, num_pkts + 1), int(num_pkts * self.data_err))
             # Need new packet making function with seqnum and checksum
             packet = num_pkts
             print("SERVER | Sending image packets...")
@@ -54,6 +59,11 @@ class Server(Thread):
                 """
                 # Change to new packet making function
                 packet = img.read(self.packet_size)
+                if counter in err_pkts:
+                    # Corrupt the data
+                    err_pkts.remove(counter) # remove from list so it doesn't loop infinitely
+                    pass
+                counter += 1 # Only increase counter on good ACK
                 sleep(0.05)
         print("SERVER | Transmission completed")
 
@@ -74,12 +84,13 @@ class Client(Thread):
         self.client_socket = socket(AF_INET, SOCK_DGRAM)
         self.client_socket.settimeout(2)
         self.packet_size = 1024
-        self.ack_err = ack_err
+        self.ack_err = ack_err / 100
 
     def recv_img(self):
         data = b''
         len = 0
         counter = 0
+        err_pkts = []
         while True:
             if counter == (len + 1):
                 break
@@ -94,9 +105,15 @@ class Client(Thread):
             """
             if len == 0:
                 len = int(packet.decode())
-            if counter:
+                # List of packets to send bad ACK:
+                err_pkts = random.sample(range(1, len + 1), int(len * self.ack_err))
+            if counter in err_pkts:
+                # Switch to bad seqnum (NACK)
+                err_pkts.remove(counter) # remove from list so it doesn't loop infinitely
+                pass
+            elif counter != 0: # Dont add first packet to data as it is the number of packets
                 data += packet
-            counter += 1
+            counter += 1 # Only increase counter on good data
 
         with open('server_to_client_image.bmp', 'wb+') as img:
             img.write(data)
